10c1584318e7cd3eb19992364444a0baa1fa54a6
Jasdeep Singh <jasi5951@colorado.edu>
2025-01-09 14:12:28 -0700

    fixing home page images

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,10 +2,10 @@ backgroundpicture = document.getElementById("hero");
 randomint = Math.floor(Math.random() * 2);
 if (window.innerWidth > 750) {
   if (randomint == 0) {
-    backgroundpicture.style.background = 'linear-gradient(rgba(55, 142, 181, 0.5), rgba(55, 142, 181, 0.2)), url("assets/img/Pricipal Dayal Singh.png")';
+    backgroundpicture.style.background = 'linear-gradient(rgba(55, 142, 181, 0.5), rgba(55, 142, 181, 0.2)), url("assets/img/PricipalDayalSingh.png")';
     backgroundpicture.style.backgroundSize = 'cover';
   } else {
-    backgroundpicture.style.background = 'linear-gradient(rgba(55, 142, 181, 0.5), rgba(55, 142, 181, 0.2)), url("assets/img/Pricipal Dayal Singh.png")';
+    backgroundpicture.style.background = 'linear-gradient(rgba(55, 142, 181, 0.5), rgba(55, 142, 181, 0.2)), url("assets/img/PricipalDayalSingh.png")';
     backgroundpicture.style.backgroundSize = 'cover';
   }
 } else {
